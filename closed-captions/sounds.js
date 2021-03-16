aac3ef59ea0239ba63410e9cddd1b8385f1c2d82
nancy-herwegh <79917607+nancy-herwegh@users.noreply.github.com>
2021-03-16 14:03:05 +0100

    Aangepaste geluiden

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,22 @@
 // Hier kan je tijden toevoegen in secondes
-// Op die mommenten komt er een class op de body.
+// Op die momenten komt er een class op de body.
 sounds = [
 	0,
 	9,
+	10,
 	12,
-	15,
+	13,
 	16.5,
-	24,
-	25.8,
-	31,
+	17.5,
+	20,
+	26,
+	27,
 	32,
-	34,
-	86
+	33,
+	49,
+	72,
+	76, /*sound 14*/
+	86,
+	88,
+	
 ];
--- closed-captions/yt.js
+++ closed-captions/yt.js
@@ -2,8 +2,9 @@ var player;
 
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('video-placeholder', {
-		width: 600,
+		width: 800,
 		height: 400,
+		
 		videoId: 'vrP-_T-h9YM',
 		playerVars: {
 			color: 'white',
